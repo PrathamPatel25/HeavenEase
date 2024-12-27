162f3454dc71bf67afd143ec415c7d99b8b81d93
PrathamPatel25 <prathampatel5044@gmail.com>
2024-12-27 11:12:15 +0530

    update/fix UI

--- routes/review.js
+++ routes/review.js
@@ -1,14 +1,28 @@
 const express = require("express");
-const router = express.Router({mergeParams: true});
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
 
 const reviewController = require("../controllers/reviews.js");
 
 //  Post Review Route
-router.post("/", isLoggedIn, validateReview, wrapAsync(reviewController.createReview));
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
 
 // Delete Review Route
-router.delete("/:reviewId", isLoggedIn, isReviewAuthor, wrapAsync(reviewController.destroyReview))
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  wrapAsync(reviewController.destroyReview)
+);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,19 +6,27 @@ const wrapAsync = require("../utils/wrapAsync");
 const passport = require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 
-const userController = require("../controllers/users")
+const userController = require("../controllers/users");
 
-router.route("/")
-    .get(userController.redirect);
+router.route("/").get(userController.redirect);
 
-router.route("/signup")
-    .get(userController.renderSignupForm)
-    .post(wrapAsync(userController.signup));
+router
+  .route("/signup")
+  .get(userController.renderSignupForm)
+  .post(wrapAsync(userController.signup));
 
-router.route("/login")
-    .get(userController.renderLoginForm)
-    .post(saveRedirectUrl, passport.authenticate("local", { failureRedirect: '/login', failureFlash: true }), userController.login);
+router
+  .route("/login")
+  .get(userController.renderLoginForm)
+  .post(
+    saveRedirectUrl,
+    passport.authenticate("local", {
+      failureRedirect: "/login",
+      failureFlash: true,
+    }),
+    userController.login
+  );
 
-router.get("/logout", userController.logout)
+router.get("/logout", userController.logout);
 
-module.exports = router
+module.exports = router;
